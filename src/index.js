a041ed2ecfb7653823ea0b48bde49a42247488ce
Mayamee <ponyashcat228@gmail.com>
2022-11-04 21:15:46 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App'
 
-const container = document.getElementById("root");
-createRoot(container).render(<App />);
+const container = document.getElementById('root')
+createRoot(container).render(<App />)
